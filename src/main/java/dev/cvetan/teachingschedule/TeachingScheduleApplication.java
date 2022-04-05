f9ad09ea3fb53b54fa24934b9b0dfc810fd919de
Cvetan Šimšić <cvetan.simsic@gmail.com>
2022-04-06 01:40:18 +0200

    Initialize project

---
+++ src/main/java/dev/cvetan/teachingschedule/TeachingScheduleApplication.java
@@ -0,0 +1,13 @@
+package dev.cvetan.teachingschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachingScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cvetan/teachingschedule/TeachingScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cvetan.teachingschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
