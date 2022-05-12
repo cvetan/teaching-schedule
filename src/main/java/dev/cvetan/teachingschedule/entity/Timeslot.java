c81e9d9db6275dafa022e729e584f7217b5f08cc
Cvetan Šimšić <cvetan.simsic@gmail.com>
2022-05-12 03:24:19 +0200

    Add Timeslot entity

---
+++ src/main/java/dev/cvetan/teachingschedule/entity/Timeslot.java
@@ -0,0 +1,31 @@
+package dev.cvetan.teachingschedule.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "timeslots")
+public class Timeslot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "day_of_week", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek dayOfWeek;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalTime endTime;
+
+}
