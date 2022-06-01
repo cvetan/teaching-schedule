12a77007166cd24706a6addf80ff6f76bb0c9d16
Cvetan Šimšić <cvetan.simsic@gmail.com>
2022-06-01 02:26:41 +0200

    Add some hard constraints

--- src/main/java/dev/cvetan/teachingschedule/solver/TimeTableConstraintProvider.java
+++ src/main/java/dev/cvetan/teachingschedule/solver/TimeTableConstraintProvider.java
@@ -6,33 +6,21 @@ import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.Joiners;
-import org.optaplanner.core.api.score.stream.tri.TriConstraintCollector;
 
-import java.util.stream.Collectors;
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.count;
 
-import static org.optaplanner.core.api.score.stream.ConstraintCollectors.toList;
-
+@SuppressWarnings("unused")
 public class TimeTableConstraintProvider implements ConstraintProvider {
 
     @Override
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
-        return new Constraint[] {
-                roomConflict(constraintFactory),
+        return new Constraint[]{
                 studentGroupConflict(constraintFactory),
-                roomCapacity(constraintFactory)
+                roomCapacity(constraintFactory),
+                maxLessonsForStudentGroup(constraintFactory),
+                subjectConflict(constraintFactory),
+                lessonTypeConflict(constraintFactory)
         };
-    }
-
-    private Constraint roomConflict(ConstraintFactory constraintFactory) {
-        return constraintFactory
-                // Select each pair of 2 different lessons ...
-                .forEachUniquePair(Lesson.class,
-                        // ... in the same timeslot ...
-                        Joiners.equal(Lesson::getTimeslot),
-                        // ... in the same room ...
-                        Joiners.equal(Lesson::getClassroom))
-                // ... and penalize each pair with a hard weight.
-                .penalize("Room conflict", HardSoftScore.ONE_HARD);
     }
 
     private Constraint studentGroupConflict(ConstraintFactory constraintFactory) {
@@ -40,23 +28,46 @@ public class TimeTableConstraintProvider implements ConstraintProvider {
         return constraintFactory
                 .forEachUniquePair(Lesson.class,
                         Joiners.equal(Lesson::getTimeslot),
-                        Joiners.equal(Lesson::getStudentGroup))
+                        Joiners.equal(Lesson::getStudentGroup)
+                )
                 .penalize("Student group conflict", HardSoftScore.ONE_HARD);
     }
 
     private Constraint roomCapacity(ConstraintFactory constraintFactory) {
-        var roomCapacity = 4;
+        var roomCapacity = 1;
 
         return constraintFactory
                 .forEach(Lesson.class)
-                .groupBy(Lesson::getTimeslot, Lesson::getClassroom, toList())
-                .filter(((timeslot, classroom, lessons) -> {
-                    var studentGroups = lessons.stream()
-                            .map(Lesson::getStudentGroup)
-                            .collect(Collectors.toSet());
-
-                    return studentGroups.size() <= roomCapacity;
-                }))
+                .groupBy(Lesson::getTimeslot, Lesson::getClassroom, Lesson::getStudentGroup, count())
+                .filter(((timeslot, classroom, lessons, count) -> count > roomCapacity))
                 .penalize("Room capacity exceeded", HardSoftScore.ONE_HARD);
+    }
+
+    private Constraint maxLessonsForStudentGroup(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Lesson.class)
+                .groupBy(Lesson::getStudentGroup, lesson -> lesson.getTimeslot().getDayOfWeek(), count())
+                .filter(((studentGroup, dayOfWeek, count) -> count > 3))
+                .penalize("Max lessons for student group", HardSoftScore.ONE_HARD);
+    }
+
+    private Constraint subjectConflict(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Lesson.class)
+                .join(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.equal(Lesson::getClassroom))
+                .filter((lesson1, lesson2) -> !lesson1.getSubject().equals(lesson2.getSubject()))
+                .penalize("Subject conflict", HardSoftScore.ONE_HARD);
+    }
+
+    private Constraint lessonTypeConflict(ConstraintFactory constraintFactory) {
+        return constraintFactory
+                .forEach(Lesson.class)
+                .join(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.equal(Lesson::getClassroom))
+                .filter((lesson1, lesson2) -> !lesson1.getLessonType().equals(lesson2.getLessonType()))
+                .penalize("Lesson type conflict", HardSoftScore.ONE_HARD);
     }
 }
